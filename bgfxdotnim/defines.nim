e507eba9d8184b70971791ffc0482f6fc675c1a0
carterza <carterza@gmail.com>
2019-08-20 22:29:35 +0300

    committed bad code

--- bgfxdotnim/defines.nim
+++ bgfxdotnim/defines.nim
@@ -406,7 +406,7 @@ const
   BGFX_BUFFER_COMPUTE_WRITE* = (0x00000200) ## !< Buffer will be used for writing.
   BGFX_BUFFER_DRAW_INDIRECT* = (0x00000400) ## !< Buffer will be used for storing draw indirect commands.
   BGFX_BUFFER_ALLOW_RESIZE* = (0x00000800) ## !< Allow dynamic index/vertex buffer resize during update.
-  BGFX_BUFFER_INDEX32* = (0x00001000'u64) ## !< Index buffer contains 32-bit indices.
+  BGFX_BUFFER_INDEX32* = (0x00001000) ## !< Index buffer contains 32-bit indices.
 
 ## /
 
